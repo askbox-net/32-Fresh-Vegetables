aa686bc3c83183baa0e038d7fe23614d369a91d1
bheungma <bheungma@students.42tokyo.jp>
2022-06-10 19:26:26 +0900

    edit some

--- raw_data/train.py
+++ raw_data/train.py
@@ -12,8 +12,66 @@ def load_pickle(pickle_file):
     return None
 
 
-def get_vec(area, ymd):
-    return weathers_weekly[area].query(f'ymd <= "{ymd}"').head(5).T
+def get_vec(x):
+    area = x['area']
+    ymd = x['ymd']
+    areas = area.split('_')
+    tmps = []
+    m = {'アメリカ': '全国',
+        'カナダ': '全国',
+        'トンガ': '全国',
+        'ニュージーランド': '全国',
+        'メキシコ': '全国',
+        '中国': '全国',
+        '佐賀': '佐賀',
+        '兵庫': '神戸',
+        '北海道': '帯広',
+        '千葉': '千葉',
+        '各地': '全国',
+        '和歌山': '和歌山',
+        '国内': '全国',
+        '埼玉': '熊谷',
+        '宮城': '仙台',
+        '宮崎': '宮崎',
+        '山形': '山形',
+        '山梨': '甲府',
+        '岩手': '盛岡',
+        '徳島': '徳島',
+        '愛媛': '松山',
+        '愛知': '名古屋',
+        '新潟': '新潟',
+        '東京': '東京',
+        '栃木': '宇都宮',
+        '沖縄': '那覇',
+        '熊本': '熊本',
+        '神奈川': '横浜',
+        '福岡': '福岡',
+        '福島': '福島',
+        '秋田': '秋田',
+        '群馬': '前橋',
+        '茨城': '水戸',
+        '長崎': '長崎',
+        '長野': '長野',
+        '青森': '青森',
+        '静岡': '浜松',
+        '香川': '高松',
+        '高知': '高知',
+        '鹿児島': '鹿児島'}
+
+    for area in areas:
+        #print('area: ', area)
+        if area in m.keys():
+            area = m[area]
+        tmps.append(weathers_weekly[area].query(f'ymd <= "{ymd}"').head(5).values.reshape(-1))
+    vs = np.mean(tmps, axis=0).tolist()
+    for k, v in zip([f"v_{i}" for i in range(len(vs))], vs):
+        x[k] = v
+
+    #return weathers_weekly[area].query(f'ymd <= "{ymd}"').head(5).values.reshape(-1)
+    #print('len: ', len(tmps))
+    #return pd.DataFrame(np.mean(tmps, axis=0))
+    #return pd.DataFrame(np.mean(tmps, axis=0))
+    return x
 
 
 if __name__ == '__main__':
@@ -21,17 +79,47 @@ if __name__ == '__main__':
     csv_weather = './weather.csv'
     csv_train = './train.csv'
     csv_test = './test.csv'
+    weathers_weekly = load_pickle(pickle_weather_weekly)
+    """
+    print(get_vec('千葉_東京', '2019-01-01'))
+    print(get_vec('千葉', '2019-01-01'))
+    print(get_vec('東京', '2019-01-01'))
+
+    print(get_vec('千葉_仙台', '2019-01-01'))
+    print(get_vec('千葉', '2019-01-01'))
+    print(get_vec('仙台', '2019-01-01'))
+    """
+
+    test_df = pd.read_csv(csv_test)
+    test_df['ymd'] = pd.to_datetime(test_df['date'], format='%Y%m%d')
+
+    print(test_df)
+    df = test_df.apply(lambda x: get_vec(x), axis=1)
+
+    #print(df.values)
+    #print(df.values.shape)
+
+    print(df)
+    print(df.shape)
+
+    df.to_csv('test_pre2.csv')
+    exit(0)
 
     train_df = pd.read_csv(csv_train)
     train_df['ymd'] = pd.to_datetime(train_df['date'], format='%Y%m%d')
 
-    weathers_weekly = load_pickle(pickle_weather_weekly)
     print(train_df)
 
-    df = train_df.query('area=="千葉"').apply(lambda x: get_vec(x['area'], x['ymd']), axis=1)
+    #df = train_df.query('area=="千葉"').apply(lambda x: get_vec(x), axis=1)
+    df = train_df.apply(lambda x: get_vec(x), axis=1)
+
+    #print(df.values)
+    #print(df.values.shape)
 
     print(df)
+    print(df.shape)
 
+    df.to_csv('train_pre2.csv')
 
     #print(weathers_weekly)
 """
---
+++ raw_data/train3.py
@@ -0,0 +1,147 @@
+# -*- coding:utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import lightgbm as lgb
+from sklearn.model_selection import train_test_split
+import os
+import pickle
+
+SEED = 42
+model_file = './models.pickle'
+
+def save_pickle(pickle_file, obj):
+    with open(pickle_file, 'wb') as fp:
+        pickle.dump(obj, fp)
+
+def load_pickle(pickle_file):
+    if os.path.exists(pickle_file):
+        with open(pickle_file, 'rb') as fp:
+            return pickle.load(fp)
+    return None
+
+params = {
+    'objective': 'regression',
+    'metric': 'rmse',
+    'num_leaves': 42,
+    'max_depth': 9,
+    "feature_fraction": 0.8,
+    'subsample_freq': 1,
+    "bagging_fraction": 0.95,
+    'min_data_in_leaf': 2,
+    'learning_rate': 0.1,
+    "boosting": "gbdt",
+    "lambda_l1": 0.1,
+    "lambda_l2": 10,
+    "verbosity": -1,
+    "random_state": 42,
+    "num_boost_round": 50000,
+    "early_stopping_rounds": 1000
+}
+
+def train():
+    csv_train2 = './train_pre2_100.csv'
+    csv_train2 = './train_pre2_10000.csv'
+    csv_train2 = './train_pre2.csv'
+
+    train_df = pd.read_csv(csv_train2)
+
+    kinds = train_df['kind'].unique()
+
+    print(kinds)
+    models = {}
+
+    for kind in kinds:
+        print(kind)
+        df = train_df.query(f'kind=="{kind}"').copy()
+
+        target = df['mode_price']
+
+        dcols = ['Unnamed: 0', 'date', 'amount', 'area', 'ymd', 'mode_price', 'kind']
+        df.drop(dcols, axis=1, inplace=True)
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            df,
+            target,
+            test_size=0.1,
+            shuffle=True,
+            random_state=SEED
+        )
+
+        tra_data = lgb.Dataset(X_train, label=y_train)
+        val_data = lgb.Dataset(X_test, label=y_test)
+
+        model = lgb.train(
+            params,
+            tra_data,
+            #categorical_feature = cat_cols,
+            valid_names = ['train', 'valid'],
+            valid_sets =[tra_data, val_data],
+            verbose_eval = 100,
+        )
+
+        #val_pred = model.predict(val_x, num_iteration=model.best_iteration)
+
+        """
+        model = lgb.LGBMRegressor(
+                n_estimators=1000,
+                random_state = SEED,
+        )
+
+        model.fit(
+            X_train, 
+            y_train,
+            eval_set=[(X_test, y_test), (X_train, y_train)]
+            #verbose=-1 # 学習ログを省略
+        )
+        """
+
+        # 学習履歴の表示
+        #lgb.plot_metric(model)
+
+        #print(model.predict(X_test))
+        models[kind] = model
+
+    save_pickle('models.pickle', models)
+
+def predict():
+    models = load_pickle(model_file)
+    csv_test2 = './test_pre2.csv'
+
+    csv_sub = './sample_submission.csv'
+
+    test_df = pd.read_csv(csv_test2)
+    sub_df = pd.read_csv(csv_sub)
+
+    kinds = test_df['kind'].unique()
+
+    #dcols = ['Unnamed: 0', 'date', 'amount', 'area', 'ymd', 'mode_price', 'kind']
+    print(kinds)
+    predicts = []
+
+    for kind in kinds:
+        print(kind)
+        x = test_df.query(f'kind=="{kind}"')
+        dcols = ['Unnamed: 0', 'date', 'area', 'ymd', 'kind']
+        x.drop(dcols, axis=1, inplace=True)
+        model = models[kind]
+        predict = model.predict(x, num_iteration=model.best_iteration)
+        print(predict)
+        print(predict.shape)
+        predicts.extend(predict)
+
+    print(predicts)
+    print(len(predicts))
+
+    #kind,date,mode_price
+    sub_df['mode_price'] = np.array(predicts)
+
+    import datetime
+    ymd_hms = datetime.datetime.today().strftime("%Y%m%d_%H%M%S")
+
+    sub_df.to_csv(f'sub_{ymd_hms}.csv', index=False)
+
+
+if __name__ == '__main__':
+    train()
+    predict()
